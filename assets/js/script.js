0005e70f15a2e6728cdcf3016f9fc102f84ee416
fabiopisa <pisanellifabio@gmail.com>
2021-04-06 14:52:31 +0200

    commit esercizio fatto

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var nome = prompt('come ti chiami?');
+var cognome = prompt('il tuo cognome?');
+var colore = prompt('colore preferito?');
+
+document.getElementById('password').innerHTML = nome + cognome + colore + "21"
